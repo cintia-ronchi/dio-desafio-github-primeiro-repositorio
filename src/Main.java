bd90018407d45b6b95b312de65e0d857d94dad5b
cintia-ronchi <cintiaronchi10@hotmail.com>
2022-05-11 13:06:52 -0300

    Inclusão do projeto de calculadora/ Java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import dio.calculadora.java.Calculadora;
+
+public class Main {
+    public static void main(String[] args) {
+       Calculadora.soma(7,8);
+       Calculadora.subtracao(15,8);
+       Calculadora.multiplicacao(7,6);
+       Calculadora.divisao(45,5);
+    }
+}
